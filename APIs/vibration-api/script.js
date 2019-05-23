cf705b81f369410198e79109e4974a6c4e2e3ef3
Pavel Ulanenko <39810676+PavloUlanenko@users.noreply.github.com>
2019-05-24 01:01:09 +0300

    vibration changes

--- APIs/vibration-api/script.js
+++ APIs/vibration-api/script.js
@@ -3,20 +3,29 @@ window.addEventListener("load", function() {
 		// let vibrateObj = {
 		// 	oneSec = 
 		// };
-		function vibrate(time=0) {
-			navigator.vibrate([1000,2000,3000]);
-			// navigator.vibrate(time);
-		}
+		// function vibrate(time=0) {
+		// 	navigator.vibrate(time);
+		// }
 		let form = document.querySelector("form");
+		let timer;
 		form.addEventListener("click", function(e) {
 			e.preventDefault();
 			let result = e.target.dataset.pattern.split(",")
 			for(let i=0; i<result.length;i++) {
 				result[i] = +result[i];
 			}
+			if(e.target.classList.contains("perpetually")) {
+				timer = setInterval(function() {
+					navigator.vibrate(result);
+				}, 1000);
+				return timer;
+			}
+			if(timer && e.target.classList.contains("stop")) {
+				clearInterval(timer);
+				navigator.vibrate(0);
+				return;
+			}
 			navigator.vibrate(result);
-			console.log(result);
-			console.log(e.target);
 		});
 	})();
 
