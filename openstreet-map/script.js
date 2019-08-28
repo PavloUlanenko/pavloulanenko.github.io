bb8d3cb6311a5968f4c9e1f8dea085d74ec4ebeb
Pavel Ulanenko <39810676+PavloUlanenko@users.noreply.github.com>
2019-08-28 15:28:29 +0300

    new

---
+++ openstreet-map/script.js
@@ -0,0 +1,70 @@
+ window.addEventListener('load', function() {
+  (function() {
+    let coordsRaw = {};
+    let initialPosition;
+    let mapContainer = document.getElementById('osm-map');
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getPosition, handleError);
+    }else {
+      mapContainer.innerText = 'Update your browser. It does not support geolocation API';
+    }
+      
+    function getPosition(position) {
+      coordsRaw.lat = +position.coords.latitude;
+      coordsRaw.lng = +position.coords.longitude;
+      initialPosition = Object.assign({}, coordsRaw);//to make an independent clone of the object
+      drawMap();
+    }
+    function handleError(error) {
+      let errors = {
+        0: 'An unknown error occured',
+        1: 'You have denied the permission to watch your position',
+        2: 'Currently we cannot track your position. Try later',
+        3: 'The tracking request has taken too much time'
+      };
+      mapContainer.innerText = errors[error.code];
+    }
+    function drawMap() {
+      let map = L.map(mapContainer);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'OpenStreetMap Pavel Ulanenko'}).addTo(map);
+      let target = L.latLng(coordsRaw.lat, coordsRaw.lng);
+      map.setView(target, 19);//zoom 19 is max in Openstreet maps
+      L.marker(target).addTo(map);//place the marker
+      // setInterval(move, 2000);
+              function move() {
+         coordsRaw.lat += .0001;
+         coordsRaw.lng += .0001;
+         target = L.latLng(coordsRaw.lat, coordsRaw.lng);
+         console.log(coordsRaw.lat);
+          map.setView(target);
+          L.marker(target).addTo(map);
+          console.log(computeDistance(initialPosition, coordsRaw));
+          let distance = `You walked: ${computeDistance(initialPosition, coordsRaw)} km`;
+          result.innerHTML = distance;
+        }
+    }
+    function observeChanges() {
+
+    }
+
+function saveProgress() {
+  localStorage.setItem('progress', JSON.stringify(document.querySelector('#osm-map').cloneNode(true)));
+}
+document.querySelector('.save-progress').onclick = saveProgress;
+  function computeDistance(startCoords, destCoords){
+    let startLatRads = degreesToRadians(startCoords.lat);
+    let startLongRads = degreesToRadians(startCoords.lng);
+    let destLatRads = degreesToRadians(destCoords.lat);
+    let destLongRads = degreesToRadians(destCoords.lng);
+    const Radius = 6371;//Earth radius in km
+    let distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads)+ Math.cos(startLatRads)*Math.cos(destLatRads)*Math.cos(startLongRads-destLongRads))*Radius;
+    return distance;
+  }
+  function degreesToRadians(degrees){
+    let radians = (degrees * Math.PI) / 180;
+    return radians;
+  }
+let result = document.querySelector('.result');
+  })();
+ });
